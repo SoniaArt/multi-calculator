3210c0818c4efe2fafc168cfad24fff3073ef4d5
Sonia <sofaartyushina@gmail.com>
2024-04-08 00:24:01 +0300

    изменённый main и калькулятор теор.вер

---
+++ enc_temp_folder/2f5a613b95dc048803dd442e288fcb4/calc2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <stdbool.h>
+#include <string.h>
+#include <locale.h>
+#include "Header2.h"
+void calc2() {}
--- multi-calculator/multi-calculator.cpp
+++ multi-calculator/multi-calculator.cpp
@@ -9,24 +9,27 @@
 #include "Header5.h"
 #include "Header6.h"
 int main() {
-	setlocale(LC_ALL, "Rus");
-	char t;
-	printf("Каким видом калькулятора вы хотите пользоваться?\n");
-	printf("1.Матричный калькулятор\n2.Калькулятор обыкновенных дробей\n3.Калькулятор формул комбинаторики\n4.Калькулятор многочленов\n5.Теор.вер. и мат.статистика\n6.Работа с функциями\n");
-	printf("Для выхода введите 0.\n");
-	do {
-		printf("Ваш выбор: "); scanf_s("%c", &t);
-		if (t < 48 || t > 55) { printf("Некорректный ввод. Повторите попытку\n"); getchar(); }
-	} while (t < 48 || t > 55);
-	switch (t) {
-	case 1: calc1(); break;
-	case 2: calc2(); break;
-	case 3: calc3(); break;
-	case 4: calc4(); break;
-	case 5: calc5(); break;
-	case 6: calc6(); break;
-	case 0: printf("Выход"); break;
-	}
-	return 0;
+    setlocale(LC_ALL, "Rus");
+    char t;
+    do {
+        printf("Каким видом калькулятора вы хотите пользоваться?\n");
+        printf("1.Матричный калькулятор\n2.Калькулятор обыкновенных дробей\n3.Калькулятор формул комбинаторики\n4.Калькулятор многочленов\n5.Теор.вер. и мат.статистика\n6.Работа с функциями\n");
+        printf("Для выхода введите 0.\n");
+        do {
+            printf("Ваш выбор: "); scanf_s("%c", &t); getchar();
+            if (t < 48 || t > 54) { printf("Некорректный ввод. Повторите попытку\n"); getchar(); }
+        } while (t < 48 || t > 54);
+        t = t - '0';
+        switch (t) {
+        case 1: calc1(); break;
+        case 2: calc2(); break;
+        case 3: calc3(); break;
+        case 4: calc4(); break;
+        case 5: calc5(); break;
+        case 6: calc6(); break;
+        case 0: printf("Выход"); break;
+        }
+    } while (t != 0);
+    return 0;
 }
 	
