f1738dedd59280d09f7303816447b3957609403b
Sonia <sofaartyushina@gmail.com>
2024-04-08 18:53:33 +0300

    Калькулятор обыкновенных дробей

--- multi-calculator/Header2.h
+++ multi-calculator/Header2.h
@@ -1,2 +1,12 @@
 #pragma once
+#define MaxN 100
+struct fraction;
+void input(char* str, int len, char* str_num, char* str_den);
+int check(char* str, int len);
+int NOD(int a, int b);
+int NOK(int a, int b);
+void comparison(fraction fr1, fraction fr2);
+void reduction(fraction fr1);
+void decimal(fraction fr1);
+void choice(fraction res);
 void calc2();
--- multi-calculator/multi-calculator.cpp
+++ multi-calculator/multi-calculator.cpp
@@ -8,25 +8,29 @@
 #include "Header4.h"
 #include "Header5.h"
 #include "Header6.h"
+#define MaxN 100
 int main() {
 	setlocale(LC_ALL, "Rus");
 	char t;
-	printf("Каким видом калькулятора вы хотите пользоваться?\n");
-	printf("1.Матричный калькулятор\n2.Калькулятор обыкновенных дробей\n3.Калькулятор формул комбинаторики\n4.Калькулятор многочленов\n5.Теор.вер. и мат.статистика\n6.Работа с функциями\n");
-	printf("Для выхода введите 0.\n");
 	do {
-		printf("Ваш выбор: "); scanf_s("%c", &t);
-		if (t < 48 || t > 55) { printf("Некорректный ввод. Повторите попытку\n"); getchar(); }
-	} while (t < 48 || t > 55);
-	switch (t) {
-	case 1: calc1(); break;
-	case 2: calc2(); break;
-	case 3: calc3(); break;
-	case 4: calc4(); break;
-	case 5: calc5(); break;
-	case 6: calc6(); break;
-	case 0: printf("Выход"); break;
-	}
+		printf("Каким видом калькулятора вы хотите пользоваться?\n");
+		printf("1.Матричный калькулятор\n2.Калькулятор обыкновенных дробей\n3.Калькулятор формул комбинаторики\n4.Калькулятор многочленов\n5.Теор.вер. и мат.статистика\n6.Работа с функциями\n");
+		printf("Для выхода введите 0.\n");
+		do {
+			printf("Ваш выбор: "); scanf_s("%c", &t); getchar();
+			if (t < 48 || t > 54) { printf("Некорректный ввод. Повторите попытку\n"); getchar(); }
+		} while (t < 48 || t > 54);
+		t = t - '0';
+		switch (t) {
+		case 1: calc1(); break;
+		case 2: calc2(); break;
+		case 3: calc3(); break;
+		case 4: calc4(); break;
+		case 5: calc5(); break;
+		case 6: calc6(); break;
+		case 0: printf("Выход"); break;
+		}
+	} while (t != 0);
 	return 0;
 }
 	
