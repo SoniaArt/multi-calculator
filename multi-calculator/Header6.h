87adb0b0dcff5ccd160b10ec56e9066fcf673e00
alexachenko <161364541+alexachenko@users.noreply.github.com>
2024-04-08 22:05:31 +0300

    Update Header6.h
    добавила функции

--- multi-calculator/Header6.h
+++ multi-calculator/Header6.h
@@ -1,2 +1,13 @@
 #pragma once
 void calc6();
+double power_function(double x, double a, double b, double c, double d);
+double polynomial(double x, double *parameters, double k);
+double indicative(double x, double a, double b, double c, double d);
+double logarithmic(double x, double a, double b, double c, double d);
+double sine(double x, double a, double b, double c, double d);
+double cosine(double x, double a, double b, double c, double d);
+double f_choose(double x, double a, double b, double c, double d, int prg);
+void section(void);
+void extreme(int prg);
+void root(int prg);
+void integral(int prg);
