52aa2fb5787d982b48fe0a75c0205a061a97fd9b
alexachenko <161364541+alexachenko@users.noreply.github.com>
2024-04-08 23:19:46 +0300

    Update calc6.cpp
    дефайн какой-то добавила

--- multi-calculator/calc6.cpp
+++ multi-calculator/calc6.cpp
@@ -5,6 +5,7 @@
 #include "Header6.h"
 #include <stdio.h>
 #include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
 //                                ,                                          
 int flag;
 int prg=0;//             
