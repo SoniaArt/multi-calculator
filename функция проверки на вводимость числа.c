540cea854723c92972b09a7181a680e83bb467e3
alexachenko <161364541+alexachenko@users.noreply.github.com>
2024-04-09 20:02:55 +0300

    добавлена проверка для int
    в этом файле находятся две функции для проверки ввода значений.
    Одна - для типа int.
    Вторая - для типа double.
    
    Программист должен выбрать текст, который появится в функции для ввода значений. для этого нужно изменить строки input_str1 и input_str2 в функции main.

--- функция проверки на вводимость числа.c
+++ функция проверки на вводимость числа.c
@@ -1,21 +1,15 @@
-//
-//  функция проверки на вводимость числа.c
-//  Function
-//
-//  Created by Александра on 09.04.2024.
-//
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-double char_double (void){
+double char_double (char *input_str){
     char y[20];
     int flag=0;//обозначение того, что число полностью проверено, и оно соответствует типу double
     double y1;
     do{
         int k_points=0;//количество точек в строке
-        printf("Введите число типа double: ");scanf("%s", y);
+        printf("%s",input_str);scanf("%s", y);
         for (int i=0; i<strlen(y);i++){
             if (i==0){
                 if (y[i]!='-' && isdigit(y[i])==0)//если символ не является минусом и не является цифрой
@@ -43,15 +37,42 @@ double char_double (void){
     return y1;
 }
 
-int char_int (char *n){
-    int n1;
-    return n1;
+int char_int(char *input_str){
+    char y[20];
+    int flag=0;//обозначение того, что число полностью проверено, и оно соответствует типу int
+    int y1;
+    do{
+        printf("%s",input_str);scanf("%s", y);
+        for (int i=0; i<strlen(y);i++){
+            if (i==0){
+                if (y[i]!='-' && isdigit(y[i])==0)//если символ не является минусом и не является цифрой
+                {break;}//прерываем программу, чтобы вводить значение заново
+            }
+
+            if (i>0)
+            {
+                if (isdigit(y[i])==0)//если символ не является цифрой
+                {break;}//прерываем программу, чтобы вводить значение заново
+            }
+            if (i==strlen(y)-1)
+            {
+                    flag=1;
+                    y1=atoi(y);
+            }
+        }
+    }while (flag==0);
+    return y1;
 }
 
 int main(void){
-    char n[10]={0};//число в строковом виде
-    int n1;//число в инт
     double y;
-    y=char_double();//аналогично scanf, но с внутренней проверкой на наличие букв
+    char input_str1[50]="Введите число типа double: ";//записываем текст, который хотим видеть на прокрутке функции
+    y=char_double(input_str1);//аналогично scanf, но с внутренней проверкой на наличие букв
+    printf("y=%lf\n", y);
+    
+    char input_str2[50]="Введите число типа int: ";//записываем текст, который хотим видеть на прокрутке функции
+    int x;
+    x=char_int(input_str2);
     printf("y=%lf\n", y);
 }
+
