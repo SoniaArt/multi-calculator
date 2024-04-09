b2ddbc483c50b096374fbcea43406be177dd4642
Sonia <sofaartyushina@gmail.com>
2024-04-09 08:51:14 +0300

    рабочая прокрутка во 2 калькуляторе, но нет контроля вводимых значений

--- multi-calculator/calc2.cpp
+++ multi-calculator/calc2.cpp
@@ -51,7 +51,7 @@ void reduction(fraction fr1) {
 	struct fraction res1;
 	res1.num = fr1.num / NOD(fr1.num, fr1.den);
 	res1.den = fr1.den / NOD(fr1.num, fr1.den);
-	if (res1.den != 1) { printf("Сокращённая дробь: %d/%d\n", res1.num, res1.den); }
+	if (res1.den != 1) { printf("%d/%d\n", res1.num, res1.den); }
 	else if (res1.den == 1) { printf("Сокращённая дробь - целое число: %d\n", res1.num); }
 }
 void decimal(fraction fr1) {
@@ -76,75 +76,58 @@ void calc2() {
 		printf("\nДля выхода введите 0.\n");
 		printf("ВАЖНО! Для корректной работы всех функций калькулятора вводите минус ТОЛЬКО в числителе.\n");
 		do {
-			printf("Ваш выбор: "); scanf_s("%c", &oper); getchar();
+			printf("Ваш выбор: "); scanf_s("%c", &oper);
 			if (oper < 48 || oper > 55) { printf("Некорректный ввод. Повторите попытку\n"); }
 		} while (oper < 48 || oper > 55);
-
 		struct fraction fr1, fr2, res;
-		int len1, len2;
-		char str1[MaxN], str2[MaxN];
-		char str_num1[MaxN], str_den1[MaxN], str_num2[MaxN], str_den2[MaxN];
-		{
+		if (oper != '0') {
 			system("cls");
 			do {
 				if (oper > 48 && oper < 54) { printf("Введите 1-ую дробь: "); }
 				else { printf("Введите дробь: "); }
-				gets_s(str1);
-				len1 = strlen(str1);
-				input(str1, len1, str_num1, str_den1);
-				fr1.num = atoi(str_num1);
-				fr1.den = atoi(str_den1);
-				if (check(str1, len1) == 1) { printf("Некорректный ввод дроби.\n"); }
-				else if (fr1.den == 0) { printf("Знаменатель дроби не может быть равен нулю.\n"); }
+				scanf_s("%d/%d", &fr1.num, &fr1.den);
+				if (fr1.den == 0) { printf("Знаменатель дроби не может быть равен нулю.\n"); }
 				if (fr1.num == 0) { fr1.den = 0; printf("Дробь равна нулю. Повторите ввод.\n"); }
-			} while ((fr1.den == 0) || (check(str1, len1) == 1));
+			} while (fr1.den == 0);
 			if (oper > 48 && oper < 54) {
 				do {
 					printf("Введите 2-ую дробь: ");
-					gets_s(str2);
-					len2 = strlen(str2);
-					input(str2, len2, str_num2, str_den2);
-					fr2.num = atoi(str_num2);
-					fr2.den = atoi(str_den2);
-					if (check(str2, len2) == 1) { printf("Некорректный ввод дроби.\n"); }
-					else if (fr2.den == 0) { printf("Знаменатель дроби не может быть равен нулю.\n"); }
-					if (fr2.num == 0) { fr2.den = 0; printf("Дробь равна нулю. Повторите ввод.\n"); }
-				} while ((fr2.den == 0) || (check(str2, len2) == 1));
+					scanf_s("%d/%d", &fr2.num, &fr2.den);
+					if (fr2.den == 0) { printf("Знаменатель дроби не может быть равен нулю.\n"); }
+				} while (fr2.den == 0);
 			}
 		}
-		
 		switch (oper) {
 		case '1':
 			res.den = NOK(fr1.den, fr2.den);
 			res.num = (fr1.num * (NOK(fr1.den, fr2.den) / fr1.den) + fr2.num * (NOK(fr1.den, fr2.den) / fr2.den));
-			printf("'+' = %d/%d\n", res.num, res.den);
-			choice(res);
+			printf("'+' = ");
+			reduction(res);
 			break;
 		case '2':
 			res.den = NOK(fr1.den, fr2.den);
 			res.num = (fr1.num * (NOK(fr1.den, fr2.den) / fr1.den) - fr2.num * (NOK(fr1.den, fr2.den) / fr2.den));
-			printf("'-' = %d/%d\n", res.num, res.den);
-			choice(res);
+			printf("'-' = ");
+			reduction(res);
 			break;
 		case '3':
 			res.num = fr1.num * fr2.num;
 			res.den = fr1.den * fr2.den;
 			if (res.den < 0) { res.num *= -1; res.den *= -1; }
-			printf("'*' = %d/%d\n", res.num, res.den);
-			choice(res);
+			printf("'*' = ");
+			reduction(res);
 			break;
 		case '4':
 			res.num = fr1.num * fr2.den;
 			res.den = fr1.den * fr2.num;
 			if (res.den < 0) { res.num *= (-1); res.den *= (-1); }
-			printf("':' = %d/%d\n", res.num, res.den);
-			choice(res);
+			printf("':' = ");
+			reduction(res);
 			break;
 		case '5': comparison(fr1, fr2); break;
-		case '6': reduction(fr1); break;
+		case '6': printf("Сокращённая дробь: "); reduction(fr1); break;
 		case '7': decimal(fr1); break;
 		case '0': printf("Выход\n"); system("cls"); break;
 		}
-		res.num = 0; res.den = 0; fr1.num = 0; fr2.num = 0;
 	} while (oper != '0'); 
 }
